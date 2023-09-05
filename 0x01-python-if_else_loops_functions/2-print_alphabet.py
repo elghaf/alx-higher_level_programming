3fd4076d9b941f667ca62661f924addaa5b79353
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-05 21:34:21 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,5 +4,6 @@
 """Write a program that prints the ASCII alphabet,
 in lowercase, not followed by a new line.
 """
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for i in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(i), end="")
+
