9f37fc23e3a178f6edefa37aef2097cba6a23029
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-09-08 03:37:36 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    len = len(argv)
-    if len == 1:
+    import sys
+
+    count = len(sys.argv) - 1
+
+    if count == 0:
         print("0 arguments.")
-    elif len > 1:
-        print("{:d} argument{:s}".format(len-1, "s:" if len > 2 else ":"))
-        for i in range(1, len):
-            print("{:d}: {:s}".format(i, argv[i]))
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    len = len(argv)
-    if len == 1:
-        print(0)
-    elif len > 1:
-        total = 0
-        for i in range(1, len):
-            total += int(argv[i])
-        print("{:d}".format(total))
+    import sys
+    count = len(sys.argv) - 1
+    sum = 0
+    for i in range(count):
+        sum += int(sys.argv[i + 1])
+        print("{}".format(sum))
