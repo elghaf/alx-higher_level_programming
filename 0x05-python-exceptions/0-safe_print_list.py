1f11307995a51b9c399c149e6ee723e53671d63b
root <ay.elghafraoui@gmail.com>
2023-09-25 09:41:43 -0700

    Default commit message

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(listo=[], cost = 0):
+def safe_print_list(my_list=[], x=0):    
     begining = 0
-    for beg in range(cost):
+    for beg in range(x):
         try:
-            print(listo[beg], end="")
+            print(my_list[beg], end="")
             begining += 1
         except IndexError:
             break
