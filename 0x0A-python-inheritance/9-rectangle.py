9afcff666cdf23fd7f7182d1741c804721af3532
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-10 15:37:39 +0100

    add

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,7 +7,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle subclass"""
-
     def __init__(self, width, height):
         """initialization method"""
         self.integer_validator('width', width)
