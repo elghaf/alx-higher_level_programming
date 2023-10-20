b355f9f865e9feb91ba5b73d8796863da94bdfa2
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-20 23:20:45 +0100

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,16 @@ class Base:
     def to_json_string(list_dictionaries):
         """JSON representation"""
         return json.dumps(list_dictionaries) if list_dictionaries else "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save JSON representation to file"""
+        filename = f"{cls.__name__}.json"
+        json_list = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        with open(filename, "w") as file:
+            file.write(Base.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Deserialize a JSON string to a list"""
+        return json.loads(json_string) if json_string else []
