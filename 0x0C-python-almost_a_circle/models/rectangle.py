75e47eef65fa46baebf69b3cb804a9fd930f3cd0
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-20 16:39:56 +0100

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,10 +17,10 @@ class Rectangle(Base):
         """
         Initialize Rectangle that inherits from base class.
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
