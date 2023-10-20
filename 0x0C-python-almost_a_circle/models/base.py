bacc2836d57db7784ddc4c32423258a0282be78d
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-20 16:20:54 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,11 +9,12 @@ class Base:
         __nb_objects (int): Number of how many time instantiate class Bases.
     """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     
