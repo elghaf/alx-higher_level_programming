c17f6ab361e402f5a563a20df244d81b5639a08a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-10-20 16:17:59 +0100

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
     
