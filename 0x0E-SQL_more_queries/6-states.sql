d45aecc92d881940722dfb6f0bb7eed8cb8f6886
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-11-20 22:57:24 +0100

    add

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- Write a script that creates the table force_name on your MySQL server.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 id INT UNIQUE AUTO_INCREMENT NOT NULL,
